907339561dd2d1308d2f392e5cc71bfb66451e42
tom-leamon <leamon@outlook.com>
2021-09-02 04:16:29 -0500

    Fix start-apps.sh script

--- scripts/start-apps.sh
+++ scripts/start-apps.sh
@@ -1,3 +1,3 @@
-cd /home/administrator/wordpresto/apps/nginx && docker-compose up -p "wordpresto-nginx-proxy-manager" -d &&
-cd /home/administrator/wordpresto/apps/portainer && docker-compose up -p "wordpresto-portainer" -d &&
-cd /home/administrator/wordpresto/apps/wordpress && docker-compose up -p "wordpresto-wordpress" -d
+cd /home/administrator/wordpresto/apps/nginx && docker-compose up -d &&
+cd /home/administrator/wordpresto/apps/portainer && docker-compose up -d &&
+cd /home/administrator/wordpresto/apps/wordpress && docker-compose up -d
