11bc8241ab57f222a830d89e5bc8604a8b92a179
tom-leamon <leamon@outlook.com>
2021-09-07 18:07:43 -0500

    Create missing backup subfolders

--- scripts/create-folders.sh
+++ scripts/create-folders.sh
@@ -5,6 +5,8 @@ mkdir -p -- \
 /home/administrator/volumes/wordpresto \
 /home/administrator/backups/nginx \
 /home/administrator/backups/portainer \
-/home/administrator/backups/wordpresto \
+/home/administrator/backups/wordpresto/database \
+/home/administrator/backups/wordpresto/wp-content \
+/home/administrator/backups/wordpresto/temp/wp-content \
 /home/administrator/backups/temp
 
