d119f46dd18043f9044ca275db624e4192aa80ae
tom-leamon <leamon@outlook.com>
2022-06-07 01:26:52 -0500

    Add missing folder to create-folders.sh script

--- scripts/create-folders.sh
+++ scripts/create-folders.sh
@@ -3,10 +3,15 @@ mkdir -p -- \
 /home/administrator/volumes/nginx \
 /home/administrator/volumes/portainer \
 /home/administrator/volumes/wordpresto \
+
 /home/administrator/backups/nginx \
 /home/administrator/backups/portainer \
+
+/home/administrator/backups/temp \
+/home/administrator/backups/temp/wp-content \
+
 /home/administrator/backups/wordpresto/database \
 /home/administrator/backups/wordpresto/wp-content \
-/home/administrator/backups/wordpresto/temp/wp-content \
-/home/administrator/backups/temp
+/home/administrator/backups/wordpresto/temp/wp-content
+
 
